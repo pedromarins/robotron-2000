b856b71add2572e5efd5a17526e0b766fbcc4ae1
Pedro Marins <contato@pedromarins.com>
2022-05-02 13:03:29 -0300

    video_3.3

--- js/main.js
+++ js/main.js
@@ -4,12 +4,14 @@ const braco = document.querySelector("#braco")
 
 const controle = document.querySelector(".controle-ajuste")
 
-somar.addEventListener("click", () => {manipulaDados("somar")})
-
-subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
+controle.forEach((elemento) => { 
+    elemento.addEventListener("click", () => {
+        console.log(evento.target)
+    })
+})
 
 function manipulaDados(operacao) {
-    if(operacao === "subtrair") {
+    if(operacao === "-") {
         braco.value = parseInt(braco.value) - 1
     } else {
         braco.value = parseInt(braco.value) + 1
