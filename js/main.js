82ae42b7e7bef12e787a814fe955f3b186831253
Pedro Marins <contato@pedromarins.com>
2022-05-02 13:01:46 -0300

    video_3.2

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@ const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 
-somar.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) + 1;
-})
+const controle = document.querySelector(".controle-ajuste")
 
-subtrair.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) - 1;
-})
+somar.addEventListener("click", () => {manipulaDados("somar")})
+
+subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
+
+function manipulaDados(operacao) {
+    if(operacao === "subtrair") {
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1
+    }
+}
