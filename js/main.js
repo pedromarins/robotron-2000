262f193649e3d6fb9342a8a6c1b5e6f3c0211208
Pedro Marins <contato@pedromarins.com>
2022-05-02 12:56:13 -0300

    video_2.2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const robotron = document.querySelector("#robotron")
 
-robotron.addEventListener("click", dizOi)
+robotron.addEventListener("click", (evento) => {
+    console.log(evento)
+})
 
-function dizOi() {
-    console.log('oi')
-} 
+function dizOi(nome) {
+    console.log('Oi ' + nome)
+    console.log('Bem-vindo ao Robotron 2000')
+}
+
+dizOi("Pedro") 
