fcca0bca9ef56dc7b247546bbb647cfdc09d8f1c
Marcos Felipe S.L <marcos.lima52@fatec.sp.gov.br>
2022-12-27 23:09:19 -0300

    data attributes of javascript

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-const subtrair = document.querySelector("#subtrair")
-const somar = document.querySelector("#somar")
-const braco = document.querySelector("#braco")
+const controle = document.querySelectorAll("[data-controle]")
 
-const controle = document.querySelectorAll(".controle-ajuste")
-console.log(controle)
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+    })
+}) 
 
-somar.addEventListener("click", () => {manipulaDados("somar")})
 
-subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
-
-function manipulaDados(operacao) {
-    debugger;
-    if(operacao === "subtrair") {
+function manipulaDados(operacao, controle) {
+    const braco = controle.querySelector("[data-contador]")
+    
+    if(operacao === "-") {
         braco.value = parseInt(braco.value) - 1 
     } else {
         braco.value = parseInt(braco.value) + 1
