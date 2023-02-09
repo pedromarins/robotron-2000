592c83a5bbc0de042a1792b0e2894323323f7b0d
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2023-02-09 10:14:05 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ function manipulaDados(operacao, controle) {
     const peca = controle.querySelector("[data-contador]")
 
     if(operacao === "-") {
-        peca.value = parseInd(peca.value) - 1
+        peca.value = parseInt(peca.value) - 1
     } else {
         peca.value = parseInt(peca.value) + 1
     }
