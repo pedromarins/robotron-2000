be296bc62fe6d4232d35e6e97515ba2026a1e2cf
Cirovsky <cirovisqui@gmail.com>
2023-03-02 08:52:42 -0300

    refatorando para otimizar atribuições de funções

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const controle = document.querySelectorAll('.controle-ajuste');
 
+const operacao = (operador, contador) =>{
+    return operador == '-'? contador -= 1 : contador +=1;
+}
 controle.forEach((ctr) =>{
+    const operador = ctr.dataset.controle;
     const contador = ctr.parentNode.querySelector(".controle-contador")
-     if(ctr.innerHTML == '-'){
-        ctr.onclick = () => contador.value = `${parseInt(contador.value) -1}`;
-    }else{
-        ctr.onclick = () => contador.value =  `${parseInt(contador.value) + 1}`; 
-    }
+    ctr.onclick = () => contador.value = `${operacao(operador, parseInt(contador.value))}`
 })
