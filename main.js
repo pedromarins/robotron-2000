ad2d857a4d3e668036b3de7d4bb05ce0bd8be1ef
Cirovsky <cirovisqui@gmail.com>
2023-03-02 11:58:44 -0300

    refatorando e interligando lógica de estatísticas

--- main.js
+++ main.js
@@ -1,10 +1,62 @@
 const controle = document.querySelectorAll('.controle-ajuste');
 
-const operacao = (operador, contador) =>{
-    return operador == '-'? contador -= 1 : contador +=1;
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
 }
-controle.forEach((ctr) =>{
+
+const estatisticas = document.querySelectorAll("[data-estatistica]");
+console.log(estatisticas)
+
+const calcular = (ctr, contador) => {
+    const tipoPeca = ctr.parentNode.parentNode.querySelector(".peca-titulo").dataset.peca;
     const operador = ctr.dataset.controle;
-    const contador = ctr.parentNode.querySelector(".controle-contador")
-    ctr.onclick = () => contador.value = `${operacao(operador, parseInt(contador.value))}`
+    atualizaEstatisticas(tipoPeca, operador)
+    return operacao(operador, contador);
+}
+
+const operacao = (operador, valor, add = 1) => {
+    return operador == '-'? valor -= add: valor += add;
+}
+
+
+const atualizaEstatisticas = (tipoPeca, operador) => {
+    estatisticas.forEach(estatistica => {
+        const caracterista = estatistica.dataset.estatistica;
+        estatistica.textContent = operacao(operador, parseInt(estatistica.textContent), pecas[tipoPeca][caracterista]);
+    })
+}
+
+controle.forEach((ctr) => {
+    const contador = ctr.parentNode.querySelector(".controle-contador");
+    ctr.onclick = () => contador.value = `${calcular(ctr, parseInt(contador.value))}`;
 })
