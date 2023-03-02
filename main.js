4804b5b60c7e26fff1d685aafe877b648399bfa9
Cirovsky <cirovisqui@gmail.com>
2023-03-02 08:32:52 -0300

    aperfeiçoando lógica do contador

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 const controle = document.querySelectorAll('.controle-ajuste');
 
 controle.forEach((ctr) =>{
-    console.log(ctr.innerHTML);
+    const contador = ctr.parentNode.querySelector(".controle-contador")
      if(ctr.innerHTML == '-'){
-        console.log(ctr.nextElementSibling.value)
-        ctr.onclick = () => ctr.nextElementSibling.value = `${parseInt(ctr.nextElementSibling.value) -1}`;
+        ctr.onclick = () => contador.value = `${parseInt(contador.value) -1}`;
     }else{
-        ctr.onclick = () => ctr.previousElementSibling.value =  `${parseInt(ctr.previousElementSibling.value) + 1}`; 
+        ctr.onclick = () => contador.value =  `${parseInt(contador.value) + 1}`; 
     }
 })
