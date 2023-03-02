e10b014bf83815297fc20818b21f866f8c5c969a
Cirovsky <cirovisqui@gmail.com>
2023-03-01 22:15:19 -0300

    refatorando funções para manipulação de dados

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const controle = document.querySelectorAll('.controle-ajuste');
 
-for( let i in controle){
-
-    if(i % 2 == 0 || i == 0){
-        let valor= parseInt(controle[i].nextElementSibling.value);
-        controle[i].onclick = () => controle[i].nextElementSibling.value = `${parseInt(controle[i].nextElementSibling.value) -1}`;
+controle.forEach((ctr) =>{
+    console.log(ctr.innerHTML);
+     if(ctr.innerHTML == '-'){
+        console.log(ctr.nextElementSibling.value)
+        ctr.onclick = () => ctr.nextElementSibling.value = `${parseInt(ctr.nextElementSibling.value) -1}`;
     }else{
-        controle[i].onclick = () => controle[i].previousElementSibling.value =  `${parseInt(controle[i].previousElementSibling.value) + 1}`; 
+        ctr.onclick = () => ctr.previousElementSibling.value =  `${parseInt(ctr.previousElementSibling.value) + 1}`; 
     }
-}
+})
