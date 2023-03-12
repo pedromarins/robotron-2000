dcc7c39b118f6397f27c4d6b8d2ce8012f0d1ec6
Karen Kramek <kakakramek@gmail.com>
2023-03-12 18:59:19 -0300

    Parte 4: data-attributes

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
-const subtrair = document.querySelector('#subtrair');
-const somar = document.querySelector('#somar');
-const braco = document.querySelector('#braco');
-
-const controle = document.querySelectorAll('.controle-ajuste');
+const controle = document.querySelectorAll('[data-controle]');
 
 controle.forEach((elemento) => {
     elemento.addEventListener('click', (evento) => {
-        manipulaDados(evento.target.textContent);
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
     });
 });
 
-function manipulaDados(operacao) {
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector('[data-contador]');
+
     if (operacao === '-') {
-        braco.value = parseInt(braco.value) - 1;
+        peca.value = parseInt(peca.value) - 1;
     } else {
-        braco.value = parseInt(braco.value) + 1;
+        peca.value = parseInt(peca.value) + 1;
     }
 }
